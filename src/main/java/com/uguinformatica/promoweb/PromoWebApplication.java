96e9a0981dbe1b3276f653182603764afbe5b06e
pedro09pm <pedro09pm.principal@gmail.com>
2024-02-05 16:11:49 +0100

    Initial commit

---
+++ src/main/java/com/uguinformatica/promoweb/PromoWebApplication.java
@@ -0,0 +1,13 @@
+package com.uguinformatica.promoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uguinformatica/promoweb/PromoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uguinformatica.promoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
