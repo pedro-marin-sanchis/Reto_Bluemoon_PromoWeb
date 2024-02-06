44352d80bf57866f89cecbdc1f0a394bd95364d8
pedro09pm <pedro09pm.principal@gmail.com>
2024-02-06 15:33:50 +0100

    Scaffold finished;

--- src/main/java/com/uguinformatica/promoweb/controller/HomeController.java
+++ src/main/java/com/uguinformatica/promoweb/controller/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LandingController {
-    @GetMapping("/landing")
+public class HomeController {
+    @GetMapping({"", "/"})
     public String getLanding() {
-        return "/landing/landing";
+        return "redirect:/app";
     }
 }
